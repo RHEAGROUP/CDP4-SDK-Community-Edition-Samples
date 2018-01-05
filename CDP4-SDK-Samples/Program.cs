53556b486ea859647986fe0e66555ced3043d16e
sam.gerene <s.gerene@rheagroup.com>
2018-01-05 08:35:52 +0100

    [Add] solution and license file

---
+++ CDP4-SDK-Samples/Program.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ClasslessDtoTestFixture.cs" company="RHEA System S.A.">
+//   Copyright (c) 2017 RHEA System S.A.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CDP4.SDK.Samples
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
